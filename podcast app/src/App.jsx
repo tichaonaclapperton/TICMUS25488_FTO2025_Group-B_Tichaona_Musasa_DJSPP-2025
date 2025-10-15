5d1fa61613e52cadaabf07e51c0621acf6ff7f35
unknown <clappertontee@gmail.com>
2025-10-15 20:40:03 +0200

    imported global player and audio player to app

--- podcast app/src/App.jsx
+++ podcast app/src/App.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import ShowDetail from "./pages/ShowPage";
+import GlobalPlayer from "../src/components/GlobalPlayer";
+import { AudioProvider } from "../src/contexts/AudioContext";
 
 /**
  * Root app that provides routes for the Homepage and Show Detail pages.
  */
 export default function App() {
 	return (
-		<div className="app-root">
-			<header className="app-header">
-				<Link to="/" className="brand">
-					🎙️ Podcast App
-				</Link>
-			</header>
+		<AudioProvider>
+			<div className="app-root">
+				<header className="app-header">
+					<Link to="/" className="brand">
+						🎙️ Podcast App
+					</Link>
+				</header>
 
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/show/:id" element={<ShowDetail />} />
-			</Routes>
-		</div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/show/:id" element={<ShowDetail />} />
+				</Routes>
+				<GlobalPlayer />
+			</div>
+		</AudioProvider>
 	);
 }
