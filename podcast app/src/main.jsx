79c351d32472398aaea762069853d2ed94217ba6
unknown <clappertontee@gmail.com>
2025-10-15 20:44:12 +0200

    imported the audio provider then wrap the app with it

--- podcast app/src/main.jsx
+++ podcast app/src/main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import {AudioProvider} from "../src/contexts/AudioContext";
+
 import App from "./App";
 import "./App.css";
 
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
-		<App />
+		<AudioProvider>
+			<App />
+		</AudioProvider>
 	</BrowserRouter>
 );
+
+
