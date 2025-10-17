aab05e1bed8b101024af8d35f95ce7cf90abb1c2
unknown <clappertontee@gmail.com>
2025-10-17 08:46:27 +0200

    remove some components to be handled in app

--- podcast app/src/main.jsx
+++ podcast app/src/main.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import {AudioProvider} from "../src/contexts/AudioContext";
 
 import App from "./App";
 import "./App.css";
 
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
-		<AudioProvider>
-			<App />
-		</AudioProvider>
+		<App />
 	</BrowserRouter>
 );
-
-
