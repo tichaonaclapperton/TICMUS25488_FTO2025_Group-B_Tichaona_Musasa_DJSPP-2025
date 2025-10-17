c700bd806b86b51aeaa748c63382cbdde509d237
unknown <clappertontee@gmail.com>
2025-10-17 08:45:27 +0200

    clean the code

--- podcast app/src/App.jsx
+++ podcast app/src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import ShowDetail from "./pages/ShowPage";
-import GlobalPlayer from "../src/components/GlobalPlayer";
-import { AudioProvider } from "../src/contexts/AudioContext";
+import GlobalPlayer from "./components/GlobalPlayer";
+import { AudioProvider } from "./contexts/AudioContext";
+import FavoritesPage from "./contexts/FavoritesPage";
+import { FavoritesProvider } from "./contexts/FavoritesContext";
 
 /**
  * Root app that provides routes for the Homepage and Show Detail pages.
@@ -11,19 +13,26 @@ import { AudioProvider } from "../src/contexts/AudioContext";
 export default function App() {
 	return (
 		<AudioProvider>
-			<div className="app-root">
-				<header className="app-header">
-					<Link to="/" className="brand">
-						🎙️ Podcast App
-					</Link>
-				</header>
+			<FavoritesProvider>
+				<div className="app-root">
+					<header className="app-header">
+						<h1>🎙️ Podcast App</h1>
+						<Link to="/" className="home-link">
+							Home
+						</Link>
+						<Link to="/favorites" className="favs-link">
+							❤️ Favourites
+						</Link>
+					</header>
 
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/show/:id" element={<ShowDetail />} />
-				</Routes>
-				<GlobalPlayer />
-			</div>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/show/:id" element={<ShowDetail />} />
+						<Route path="/favorites" element={<FavoritesPage />} />
+					</Routes>
+					<GlobalPlayer />
+				</div>
+			</FavoritesProvider>
 		</AudioProvider>
 	);
 }
