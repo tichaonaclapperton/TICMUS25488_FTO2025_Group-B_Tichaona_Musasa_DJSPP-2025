77dceedc551e0eb6104a8da1c8757e86359d3d2e
unknown <clappertontee@gmail.com>
2025-10-18 11:20:56 +0200

    renamed the app folder

---
+++ podcast_app/src/utilities/GenreFilter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { genres } from "../genres/genre";
+
+/**
+ * A dropdown filter for selecting a genre.
+ *
+ * @component
+ * @param {Object} props
+ * @param {string} props.value - The currently selected genre.
+ * @param {Function} props.onChange - Callback fired when the genre selection changes.
+ * @returns {JSX.Element} A genre filter dropdown component.
+ */
+
+
+export default function GenreFilter({ value, onChange }) {
+	return (
+		<label>
+            
+			<select value={value} onChange={(e) => onChange(e.target.value)}>
+				<option value="">All Genres</option>
+				{genres.map((g) => (
+					<option key={g.id} value={g.title}>
+						{g.title}
+					</option>
+				))}
+			</select>
+		</label>
+	);
+}
